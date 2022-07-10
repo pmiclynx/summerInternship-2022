f7971059bc5c9656960938a62115e1760edee39b
Danyel Morjan <danyelmorjan@yahoo.com>
2022-07-10 15:30:14 +0300

    #1 Create Login-Register Screens: Add login screen.

--- app/src/main/java/com/summer/internship/tvtracker/MainActivity.kt
+++ app/src/main/java/com/summer/internship/tvtracker/MainActivity.kt
@@ -2,10 +2,13 @@ package com.summer.internship.tvtracker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        // TODO viewBinding
     }
 }
