daafa0bc7120e33b2e29fa59a340f99c8729464a
Danyel Morjan <danyelmorjan@yahoo.com>
2022-07-14 15:09:06 +0300

    #3 Create Bottom Navigation with 3 Fragments: created bottom nav with 3 fragments

--- app/src/main/java/com/summer/internship/tvtracker/MainActivity.kt
+++ app/src/main/java/com/summer/internship/tvtracker/MainActivity.kt
@@ -2,12 +2,27 @@ package com.summer.internship.tvtracker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.MediaController
 import android.widget.TextView
 import android.widget.Toast
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI.setupWithNavController
+
+import com.summer.internship.tvtracker.databinding.ActivityMainBinding
+import com.summer.internship.tvtracker.databinding.ActivityRegisterBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.MainContainer) as NavHostFragment
+        navController = navHostFragment.navController
+        val bottomNavigationView = binding.bottomNavigationView
+        setupWithNavController(bottomNavigationView, navController)
     }
 }
