53bc536c3a5d395168882b2b8929a77f68aa02d8
Danyel Morjan <danyelmorjan@yahoo.com>
2022-07-14 17:36:00 +0300

    #7 Add firebase login & register: added firebase login & register

--- app/src/main/java/com/summer/internship/tvtracker/Register/RegisterActivity.kt
+++ app/src/main/java/com/summer/internship/tvtracker/Register/RegisterActivity.kt
@@ -2,14 +2,20 @@ package com.summer.internship.tvtracker.Register
 
 import android.content.Intent
 import android.os.Bundle
+import android.text.TextUtils
+import android.util.Log
 import android.util.Patterns
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import com.summer.internship.tvtracker.MainActivity
 import com.summer.internship.tvtracker.databinding.ActivityRegisterBinding
 
 class RegisterActivity : AppCompatActivity() {
     private lateinit var binding: ActivityRegisterBinding
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityRegisterBinding.inflate(layoutInflater)
@@ -38,10 +44,65 @@ class RegisterActivity : AppCompatActivity() {
                     .show()
                 return@setOnClickListener
             }
-            val intent = Intent(this, MainActivity::class.java)
-            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
-            startActivity(intent)
-            finish()
+            if(pass.length<6) {
+                Toast.makeText(this, "Password must be at least 6 characters", Toast.LENGTH_SHORT)
+                    .show()
+                return@setOnClickListener
+            }
+            auth = Firebase.auth
+            createAccount(binding.editTextEmail.text.toString(), binding.editTextPassword.text.toString())
         }
+    }
+
+    private fun createAccount(email: String, password: String) {
+        Log.d(TAG, "createAccount:$email")
+        if (!validateForm()) {
+            return
+        }
+
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(TAG, "createUserWithEmail:success")
+                    val user = auth.currentUser
+                    val intent = Intent(this, MainActivity::class.java)
+                    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
+                    startActivity(intent)
+                    finish()
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(TAG, "createUserWithEmail:failure", task.exception)
+                    Toast.makeText(this, "Authentication failed.",
+                        Toast.LENGTH_SHORT).show()
+                }
+            }
+    }
+
+    private fun validateForm(): Boolean {
+        var valid = true
+
+        val email = binding.editTextEmail.text.toString()
+        if (TextUtils.isEmpty(email)) {
+            binding.editTextEmail.error = "Required."
+            valid = false
+        } else {
+            binding.editTextEmail.error = null
+        }
+
+        val password = binding.editTextPassword.text.toString()
+        if (TextUtils.isEmpty(password)) {
+            binding.editTextPassword.error = "Required."
+            valid = false
+        } else {
+            binding.editTextPassword.error = null
+        }
+
+        return valid
+    }
+
+    companion object {
+        private const val TAG = "EmailPassword"
+        private const val RC_MULTI_FACTOR = 9005
     }
 }
